7090c9e2af1457575d62a9e93346d18e2bd640d4
Jeff Sutherland <jdsuther@asu.edu>
2017-02-12 11:46:25 +0700

    Remove dead code

--- reactoring-naive-bayes-classifier/naiveBayesClassifier.js
+++ reactoring-naive-bayes-classifier/naiveBayesClassifier.js
@@ -20,7 +20,6 @@ toxic = ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7',
 bulletproof = ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'];
 
 var songs = [];
-var labels = [];
 var allChords = new Set();
 var labelCounts = {};
 var labelProbabilities = {};
@@ -29,7 +28,6 @@ var probabilityOfChordsInLabels = {};
 
 function train(chords, label){
   songs.push([label, chords]);
-  labels.push(label);
   chords.forEach(chord => allChords.add(chord));
 
   if(Object.keys(labelCounts).includes(label)){
