91e997f7be745a6bcb5ffa0a29cf63139d8ede77
Jeff Sutherland <jdsuther@asu.edu>
2017-02-15 16:31:21 +0700

    Reorder and cleanup to prepare for public api

--- reactoring-naive-bayes-classifier/nb.js
+++ reactoring-naive-bayes-classifier/nb.js
@@ -14,13 +14,8 @@ class Classifier {
     };
   }
 
-  likelihoodFromChord(difficulty, chord) {
-    return this.chordCountForDifficulty(difficulty, chord) / this.songList.songs.length;
-  }
-
-  valueForChordDifficulty(difficulty, chord) {
-    const value = this.likelihoodFromChord(difficulty, chord);
-    return value ? value + this.smoothing : 1;
+  addSong(...songParams) {
+    this.songList.addSong(...songParams);
   }
 
   classify(chords) {
@@ -30,7 +25,33 @@ class Classifier {
         return [difficulty, chords.reduce((total, chord) => (
           total * this.valueForChordDifficulty(difficulty, chord)
         ), this.labelProbabilities.get(difficulty) + this.smoothing)];
-      }));
+      })
+    );
+  }
+
+  trainAll() {
+    this.songList.songs.forEach(song => this.train(song.chords, song.difficulty));
+    this.setLabelProbabilities();
+  }
+
+  // TODO: make idempotent
+  train(chords, label) {
+    chords.forEach(chord => this.songList.allChords.add(chord));
+
+    if (Array.from(this.labelCounts.keys()).includes(label)) {
+      this.labelCounts.set(label, this.labelCounts.get(label) + 1);
+    } else {
+      this.labelCounts.set(label, 1);
+    }
+  }
+
+  valueForChordDifficulty(difficulty, chord) {
+    const value = this.likelihoodFromChord(difficulty, chord);
+    return value ? value + this.smoothing : 1;
+  }
+
+  likelihoodFromChord(difficulty, chord) {
+    return this.chordCountForDifficulty(difficulty, chord) / this.songList.songs.length;
   }
 
   chordCountForDifficulty(difficulty, testChord) {
@@ -42,25 +63,10 @@ class Classifier {
     }, 0);
   }
 
-  train(chords, label) {
-    chords.forEach(chord => this.songList.allChords.add(chord));
-
-    if (Array.from(this.labelCounts.keys()).includes(label)) {
-      this.labelCounts.set(label, this.labelCounts.get(label) + 1);
-    } else {
-      this.labelCounts.set(label, 1);
-    }
-  }
-
   setLabelProbabilities() {
     this.labelCounts.forEach((_count, label) => {
       this.labelProbabilities.set(label, this.labelCounts.get(label) / this.songList.songs.length);
     });
-  }
-
-  trainAll() {
-    this.songList.songs.forEach(song => this.train(song.chords, song.difficulty));
-    this.setLabelProbabilities();
   }
 }
 
@@ -69,25 +75,18 @@ const wish = require('wish');
 describe('the file', () => {
   const classifier = new Classifier();
 
-  classifier.songList.addSong('imagine',
-    ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
-  classifier.songList.addSong('somewhereOverTheRainbow',
-    ['c', 'em', 'f', 'g', 'am'], 0);
-  classifier.songList.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
-  classifier.songList.addSong('iWillFollowYouIntoTheDark',
-    ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
-  classifier.songList.addSong('babyOneMoreTime',
-    ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
-  classifier.songList.addSong('creep',
-    ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1);
-  classifier.songList.addSong('paperBag',
-    ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em',
-      'a', 'cmaj7', 'em7', 'a7', 'f7',
-      'b'], 2);
-  classifier.songList.addSong('toxic',
-    ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
-  classifier.songList.addSong('bulletproof',
-    ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
+  classifier.addSong('imagine', ['c', 'cmaj7', 'f', 'am', 'dm', 'g', 'e7'], 0);
+  classifier.addSong('somewhereOverTheRainbow', ['c', 'em', 'f', 'g', 'am'], 0);
+  classifier.addSong('tooManyCooks', ['c', 'g', 'f'], 0);
+  classifier.addSong('iWillFollowYouIntoTheDark', ['f', 'dm', 'bb', 'c', 'a', 'bbm'], 1);
+  classifier.addSong('babyOneMoreTime', ['cm', 'g', 'bb', 'eb', 'fm', 'ab'], 1);
+  classifier.addSong('creep', ['g', 'gsus4', 'b', 'bsus4', 'c', 'cmsus4', 'cm6'], 1);
+  classifier.addSong('paperBag', ['bm7', 'e', 'c', 'g', 'b7', 'f', 'em',
+    'a', 'cmaj7', 'em7', 'a7', 'f7',
+    'b'
+  ], 2);
+  classifier.addSong('toxic', ['cm', 'eb', 'g', 'cdim', 'eb7', 'd7', 'db7', 'ab', 'gmaj7', 'g7'], 2);
+  classifier.addSong('bulletproof', ['d#m', 'g#', 'b', 'f#', 'g#m', 'c#'], 2);
 
   classifier.trainAll();
 
