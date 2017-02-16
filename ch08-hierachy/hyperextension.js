a9e9d8e11d7429821767378b37bb977477f94dfc
Jeff Sutherland <jdsuther@asu.edu>
2017-02-16 17:03:27 +0700

    Refactor remove hierachy

--- ch08-hierachy/hyperextension.js
+++ ch08-hierachy/hyperextension.js
@@ -1,54 +1,23 @@
-// example of inheritance antipattern
 class Report {
   constructor(params) {
     this.params = params;
   };
   printReport(params) {
-    return params;
+    return Object.assign(this.params, params);
   };
 }
-
-class GenericReport extends Report {
-  constructor(params) {
-    super(params);
-    this.params = params;
-  };
-  printReport(params) {
-    return super.printReport(Object.assign(this.params, params));
-  };
-}
-
-class ClientReport extends GenericReport {
-  constructor(params) {
-    super(params);
-    this.params = params;
-  };
-  printReport(params) {
-    return super.printReport(Object.assign(this.params, params));
-  };
-}
-
-class SpecificClientReport extends ClientReport {
-  constructor(params) {
-    super(params);
-    this.params = params;
-  };
-  printReport(params) {
-    return super.printReport(Object.assign(this.params, params));
-  };
-}
-
-report = new SpecificClientReport({
-  whatever: 'we want',
-  to: 'add'
-});
 
 const wish = require('wish');
 const deepEqual = require('deep-equal');
+const report = new Report({
+  whatever: 'we want',
+  to: 'add'
+})
+
 wish(deepEqual(report.printReport({
   extra: 'params'
 }), {
   whatever: "we want",
   to: "add",
   extra: "params"
-}));
+}))
